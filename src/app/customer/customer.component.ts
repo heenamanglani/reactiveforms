be72be489bcdd4526f0593bb3461d6da6c8031f6
heenamanglani <heena.manglani3@gmail.com>
2018-09-27 16:03:19 +0200

    Fresh Commit

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -60,6 +60,8 @@ export class CustomerComponent implements OnInit {
       sendCatalog: true,
       rating: ['', ratingRange(1, 5)]
     });
+
+    this.customerForm.get('notification').valueChanges.subscribe(value => this.sendNotification(value));
   }
 
   populateTestData(): void {
